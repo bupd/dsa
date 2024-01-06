e0d03061513e2dfe83342dca8aa733a9dda5ff95
Prasanth B <89722848+bupd@users.noreply.github.com>
2024-01-06 21:43:06 +0530

    Create MaxProfitInJobScheduling.cpp

---
+++ MaxProfitInJobScheduling.cpp
@@ -0,0 +1,54 @@
+class job{
+    public:
+        int start, end, profit;
+    
+    job(int a, int b, int c){
+        start = a;
+        end = b;
+        profit = c;
+    }
+};
+
+class Solution {
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = profit.size();
+        
+        vector<job> jobs;
+        
+        for(int i = 0; i < n; i++){
+            job newJob(startTime[i], endTime[i], profit[i]);
+            jobs.emplace_back(newJob);
+        }
+        
+        sort(jobs.begin(), jobs.end(), [](job &a, job &b){
+            return a.start < b.start;    
+        });
+            
+        vector<int> dp(n + 1, 0);
+        
+        for(int i = n - 1; i > -1; i--){
+            int take, notake;
+            
+            notake = dp[i + 1];
+            
+            int low = i; // low is always false
+            int high = n; // high is always true
+            
+            while(low < high - 1){
+                int mid = low + (high - low) / 2;
+                
+                if(jobs[mid].start >= jobs[i].end)
+                    high = mid;
+                else
+                    low = mid;
+            }
+            
+            take = jobs[i].profit + dp[high];
+            
+            dp[i] = max(take, notake);
+        }
+        
+        return dp[0];
+    }
+};
